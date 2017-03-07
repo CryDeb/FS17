cddcf04ed1e6f7f6b5594a1986b23b9072621947
Dane Wicki (CryDeb) <wicki.dane@gmail.com>
2017-03-07 13:55:14 +0100

    new files

---
+++ UI-Demo_DaneWicki/app/src/main/java/ch/dane/wicki/ui_demo_danewicki/LinearLayoutDemoActivity.java
@@ -0,0 +1,13 @@
+package ch.dane.wicki.ui_demo_danewicki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearLayoutDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_linear_layout_demo);
+    }
+}
---
+++ UI-Demo_DaneWicki/app/src/main/java/ch/dane/wicki/ui_demo_danewicki/RelativeLayoutDemoActivity.java
@@ -0,0 +1,13 @@
+package ch.dane.wicki.ui_demo_danewicki;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeLayoutDemoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relative_layout_demo);
+    }
+}
